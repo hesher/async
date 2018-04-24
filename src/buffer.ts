71b913c4e960a7333e4c2bef73f3b12a68210498
Ben Katz <benk@wix.com>
2018-04-24 15:17:35 +0300

    small refactor to buffer

--- src/buffer.ts
+++ src/buffer.ts
@@ -3,29 +3,11 @@ export const buffer = (size: number) => <T>(
   curr: AggVal<T>,
   prev: AggVal<any>
 ): AggVal<any> => {
-  let ret: AggVal<any>;
   const buff = prev.props['buffering'] || 0;
-  if (buff === 0) {
-    ret = {
-      ...curr,
-      nulled: true,
-      props: {...curr.props, buffering: buff + 1},
-      val: [curr.val]
-    };
-  } else if (buff < size - 1) {
-    ret = {
-      ...curr,
-      nulled: true,
-      props: {...curr.props, buffering: buff + 1},
-      val: [...prev.val, curr.val]
-    };
-  } else {
-    ret = {
-      ...curr,
-      props: {...curr.props, buffering: 0},
-      val: [...prev.val, curr.val]
-    };
-  }
-
-  return ret;
+  return {
+    ...curr,
+    nulled: buff === size - 1 ? curr.nulled : true,
+    props: {...curr.props, buffering: buff === size - 1 ? 0 : buff + 1},
+    val: [...(buff === 0 ? [] : prev.val), curr.val]
+  };
 };
